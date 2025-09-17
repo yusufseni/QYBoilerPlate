5c81d1d9b5344cbbae9c108c9e14f25cbb141f13
Moh Yusuf Ibrahim <yusuf@softwareseni.com>
2025-09-17 12:24:45 +0700

    removed unused test and put setter getter in FlashMessage

--- src/main/java/com/yoesoff/plate/dto/FlashMessage.java
+++ src/main/java/com/yoesoff/plate/dto/FlashMessage.java
@@ -1,13 +1,27 @@
 package com.yoesoff.plate.dto;
 
+/**
+ * FlashMessage is used to represent temporary messages that can be displayed to users.
+ * It contains a type (INFO, WARNING, DANGER, SUCCESS) and the message text.
+ * Quarkus Qute does not support Java record properties as bean-style getters (like getType()).
+ * Qute expects standard getter methods or public fields
+ */
 public class FlashMessage {
-    public enum Type { INFO, WARNING, DANGER, SUCCESS }
-
-    public final Type type;
-    public final String text;
+    private final Type type;
+    private final String text;
 
     public FlashMessage(Type type, String text) {
         this.type = type;
         this.text = text;
     }
+
+    public Type getType() {
+        return type;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public enum Type {INFO, WARNING, DANGER, SUCCESS}
 }
